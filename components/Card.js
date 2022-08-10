41b3c276cf78450bc3e4c6998457ea84f1c3c10d
tylergeorges <tmg320v@gmail.com>
2022-08-10 13:11:36 -0500

    Updated CSS 🎉🎊

--- components/Card.js
+++ components/Card.js
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react"
 
-const Card = () =>{
+import { HiOutlineArrowRight } from "react-icons/hi";
+
+const Card = () => {
 
     const [cardId, setCardId] = useState(null)
     const [vocabData, setVocabData] = useState([])
-   
-    useEffect(() =>{
+
+    useEffect(() => {
 
 
         const data = [
@@ -27,40 +29,62 @@ const Card = () =>{
                 def: "numbers without a fractional component, and don't support decimal points",
                 id: 3
             },
-           
-    ]
-    setVocabData([...data])
-    // console.log(...vocabData.category)
 
-    },[])
+        ]
+        setVocabData([...data])
+        // console.log(...vocabData.category)
 
-    const toggleCard = (e) =>{
+    }, [])
+
+    const toggleCard = (e) => {
         e.preventDefault()
 
         //hides card info if it is already selected and clicked again. 
-        if(cardId == e.target.id){
+        if (cardId == e.target.id) {
             setCardId(null)
-        }else{
+        } else {
             setCardId(e.target.id)
         }
 
     }
 
-    return(
+    return (
         <div className="card-con"  >
-   
-        
+            
             {vocabData.map(keys => {
-                return(
+                return (
                     <div className="card" key={keys.keyword + keys.id + ''} onClick={toggleCard} id={keys.id}>
 
-                        <div className="big-circle"/>
-                        <div className="med-circle"/>
-                        <div className="small-circle"/>
 
-                        <h1 className="card-keyword">{keys.keyword}</h1>
+                        {cardId == keys.id ? '' :
+                            <div className="card-keyword-con">
 
-                        <p className="card-def">{cardId == keys.id ?  keys.def : ''} </p>
+                                <h1 className={cardId == keys.id ? "card-active card-keyword" : 'card-keyword'}>{keys.keyword}</h1>
+
+                            </div>
+                        }
+
+
+                        <div className={cardId == keys.id ? "active-circle-con" : 'circle-con'}>
+
+
+                            <div className={cardId == keys.id ? "active-circle" : 'big-circle'}>
+                            {/* <HiOutlineArrowRight className="circle-arrow"/> */}
+                            {cardId == keys.id ? 
+                            <div className="card-keyword-con">
+
+                                <h1 className="card-active-keyword card-keyword" >{keys.keyword}</h1>
+
+                            </div> : ''
+
+                            }
+                            </div>
+                            <p className="card-def">{cardId == keys.id ? keys.def : ''} </p>
+
+                        </div>
+
+
+
                     </div>
                 )
             })}
